041d888de7b0ca2986c55813b1af3332dae43f25
Thomas Voss <thomasvoss@live.com>
2021-12-12 11:41:01 +0100

    Initialize a window

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,6 +4,25 @@
 #include <unistd.h>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_video.h>
+
+const char *argv0;
+
+SDL_Window *
+init_window(void)
+{
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0)
+			errx(EXIT_FAILURE, "%s: failed to initialize SDL2: %s\n", argv0,
+			     SDL_GetError());
+	}
+	SDL_Window *window = SDL_CreateWindow("PID Game", SDL_WINDOWPOS_UNDEFINED,
+	                                      SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+		errx(EXIT_FAILURE, "%s: window could not be created: %s\n", argv0, SDL_GetError());
+	return window;
+}
 
 int
 main(int argc, char **argv)
@@ -19,6 +38,11 @@ main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 	}
-	
+
+	argv0 = argv[0];
+	SDL_Window *window = init_window();
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return EXIT_SUCCESS;
 }
