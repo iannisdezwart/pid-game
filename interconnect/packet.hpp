72bfa969fc0c1617a91f5f3a151f287758858f43
Arav K <A.Khanna-1@student.tudelft.nl>
2021-12-12 10:58:30 +0100

    [interconnect/packet] add header docs and license

--- interconnect/packet.hpp
+++ interconnect/packet.hpp
@@ -1,3 +1,28 @@
+/**
+ * Low-level network packet manipulation.
+ * 
+ * This module provides a packet structure as a thin layer on top of UDP.  It
+ * provides some additional information for the server-client structure of $GAME
+ * such as client-server time synchronization.
+ * 
+ * ---
+ * 
+ * Copyright 2021 Iannis de Zwart, Karol Jurski, Arav K.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef INTERCONNECT_TYPES_H
 #define INTERCONNECT_TYPES_H
 
